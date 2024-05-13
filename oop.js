90c0e956f3a174d4ed887db9c56b37a47a6f8ec3
Hiba Dawood <164556977+HibaDawood@users.noreply.github.com>
2024-05-13 21:16:05 +0500

    Add files via upload

---
+++ oop.js
@@ -0,0 +1,54 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+class Student {
+    name;
+    constructor(n) {
+        this.name = n;
+    }
+}
+class Person {
+    students = [];
+    addStudent(obj) {
+        this.students.push(obj);
+    }
+}
+const persons = new Person();
+const programmStart = async (persons) => {
+    do {
+        console.log(chalk.bold.greenBright(`\n\tWelcome guests\n\t`));
+        const ans = await inquirer.prompt({
+            name: "select",
+            type: "list",
+            message: chalk.bold.bgRedBright("Who would you like to talk to?"),
+            choices: ["by yourself", "student", "I don't want to talk to anyone"],
+        });
+        if (ans.select == "by yourself") {
+            console.log(`Hello, I am talking to myself.`);
+            console.log(`I am good!`);
+        }
+        if (ans.select == "student") {
+            const ans = await inquirer.prompt({
+                name: "student",
+                type: "input",
+                message: chalk.bold.bgYellow("Which student do you want to talk to? : "),
+            });
+            const student = persons.students.find((val) => val.name == ans.student);
+            if (!student) {
+                const name = new Student(ans.student);
+                persons.addStudent(name);
+                console.log(chalk.bold.italic.magenta(`Hello I am ${name.name}, and I am fine.`));
+                console.log(persons.students);
+            }
+            if (student) {
+                console.log(chalk.bold.magenta(`Hello I am ${student.name}, and Yeah! I am fine.....`));
+                console.log(persons.students);
+            }
+        }
+        if (ans.select == "I don't want to talk to anyone") {
+            console.log(chalk.bold.red("Alright, have a good day!"));
+            break;
+        }
+    } while (true);
+};
+programmStart(persons);
